7071b131a0f27e7115c4098587fa78c6ca3127c5
Kamran Sadin <mrsadin@gmail.com>
2021-09-18 11:50:22 +0430

    Adding projects and Dockerfiles

---
+++ src/Common/TestClass.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace Common
+{
+    public class TestClass
+    {
+        public void WriteThis(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
